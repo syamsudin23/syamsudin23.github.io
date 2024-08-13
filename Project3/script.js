95d65d79d002b957ef68164472e779558c6b5867
syamsudin23 <symsantos7@gmail.com>
2024-08-14 02:54:28 +0700

    Add files via upload

---
+++ Project3/script.js
@@ -0,0 +1,87 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
+
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+
+    
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
+
+
+ // Array of texts to type
+        let texts = ["Everyone", "tо XI гקl 6"];
+        let typingText = document.getElementById('typing-text');
+        let textIndex = 0;
+
+        function typeEffect() {
+            let textToType = texts[textIndex];
+            let charIndex = 0;
+
+            function type() {
+                let currentText = textToType.substring(0, charIndex + 1);
+                typingText.innerHTML = currentText;
+                charIndex++;
+                if (charIndex <= textToType.length) {
+                    setTimeout(type, 100);
+                } else {
+                    setTimeout(() => {
+                        textIndex = (textIndex + 1) % texts.length;
+                        typeEffect();
+                    }, 2000);
+                }
+            }
+
+            type();
+        }
+
+        window.onload = function() {
+            typeEffect();
+        };
